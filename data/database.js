1ea0ca87676fd7f438049195eb1c419c0b982062
Raj Deshmukh <rajdeshmukh41705@gmail.com>
2023-06-22 21:14:56 +0530

    database env updated

--- data/database.js
+++ data/database.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-export const database = () => { mongoose.connect("mongodb://127.0.0.1:27017", {
+export const database = () => { mongoose.connect(process.env.MONGO_URI, {
 dbName : "project1"
-}).then(() => {
-    console.log("Database is connected")
+}).then((c) => {
+    console.log(`Database is connected with ${c.connection.host}`)
 }).catch(err => console.log("db error"))
 }
