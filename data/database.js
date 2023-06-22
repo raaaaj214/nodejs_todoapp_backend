402493b82014d8e752fbc7bc9e5d280088f67abf
Raj Deshmukh <rajdeshmukh41705@gmail.com>
2023-06-22 21:05:12 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const database = () => { mongoose.connect(process.env.MONGO_URI, {
 dbName : "project1"
-}).then(() => {
-    console.log("Database is connected")
-}).catch(err => console.log("db error"))
+}).then((c) => {
+    console.log(`Database is connected with ${c.connection.host}`)
+}).catch(err => console.log("db error",err))
 }
