85194e7ed5e5247fdfdf8d5edf777819090fe3cf
Raj Deshmukh <104126185+raaaaj214@users.noreply.github.com>
2023-06-23 19:50:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.json())
 
 app.use(cors({
-    origin : [process.env.FRONTEND_URL],
+    origin : process.env.FRONTEND_URL,
     methods : ["GETS", "POST","PUT","DELETE"],
     credentials : true,
     
