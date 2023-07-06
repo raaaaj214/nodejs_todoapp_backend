651b8fcbdccb19ebbdb5f8df943180ed4866e979
Raj Deshmukh <104126185+raaaaj214@users.noreply.github.com>
2023-07-07 02:49:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.json())
 
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : 'https://nodejs-todoapp-frontend-three.vercel.app/',
     methods : ["GET", "POST","PUT","DELETE"],
     credentials : true,
     
